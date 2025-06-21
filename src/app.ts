b01e712f16fdc373aff379c1a99f9560961bcc05
Emon <emon.mhk69@gmail.com>
2025-06-22 02:49:29 +0600

    deployment done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-import express from 'express';
-import bookRoutes from './routes/book.routes';
-import borrowRoutes from './routes/borrow.routes';
-import globalErrorHandler from './middlewares/globalErrorHandler';
+import express from "express";
+import bookRoutes from "./routes/book.routes";
+import borrowRoutes from "./routes/borrow.routes";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/books', bookRoutes);
-app.use('/api/borrow', borrowRoutes);
+app.get("/", (_req, res) => {
+  res.send("Library Management Backend is running!");
+});
+
+app.use("/api/books", bookRoutes);
+app.use("/api/borrow", borrowRoutes);
 
 app.use(globalErrorHandler);
 
